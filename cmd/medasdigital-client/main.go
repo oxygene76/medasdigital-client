7904f3068d3d8d76a4d2ce1ff4a13bb32eb0ab29
MedasDigital <111655819+oxygene76@users.noreply.github.com>
2025-07-12 14:44:20 +0200

    Update main.go

--- cmd/medasdigital-client/main.go
+++ cmd/medasdigital-client/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
     	"github.com/cosmos/cosmos-sdk/client"
