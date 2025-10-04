952f953d5e14ce472bb7cba40652792358932815
MedasDigital <111655819+oxygene76@users.noreply.github.com>
2025-10-04 19:01:07 +0200

    Update client.go

--- pkg/contract/client.go
+++ pkg/contract/client.go
@@ -165,7 +165,6 @@ func (c *Client) SubmitJob(
         "--gas", "auto",
         "--gas-adjustment", "1.3",
         "--gas-prices", "0.025umedas", 
-        "--broadcast-mode", "sync",
         "-y",
         "--node", c.config.RPCEndpoint,
         "--chain-id", c.config.ChainID,
